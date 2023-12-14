f366c045ebdee2421cd993ee6be89ca9a7154c1d
Pedro Carvalho <pcardosomundim@gmail.com>
2023-12-14 12:21:06 -0300

    Seção 7

---
+++ Messkiro's Adventure/Assets/Scripts/Farm/SlotFarm.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotFarm : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite hole;
+    [SerializeField] private Sprite carrot;
+
+    [SerializeField] private int digAmount; // quantidade para criar o buraquinho
+    private int initialDigAmount;
+
+    private void Start()
+    {
+        initialDigAmount = digAmount;
+    }
+
+    public void OnHit()
+    {
+        digAmount--;
+
+        if(digAmount <= initialDigAmount / 2)
+        {
+            spriteRenderer.sprite = hole;
+        }
+
+        //if (digAmount <= 0)
+        //{
+            // plantar cenoura
+            //spriteRenderer.sprite = carrot;
+        //}
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dig"))
+        {
+            OnHit();
+        }
+    }
+}
